a401c35f4e1b37eec1a139c2aef3aa5ced4a4938
Александр Хасматулин <alexandr.khasmatulin@yandex.ru>
2023-09-25 22:07:58 +0300

    final

--- CourseWork7_DRF/settings.py
+++ CourseWork7_DRF/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'CourseWork7',
         'USER': 'postgres',
-        'PASSWORD': 'Alex030652',
+        'PASSWORD': 'nMBH8GezaE',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
